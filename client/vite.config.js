6a0ad87c65894b6e6c15c04cb07a78805f989a7e
Panther4u <teampanther4@gmail.com>
2024-08-08 13:29:54 +0530

    Update build and deployment configurations

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,26 +1,52 @@
+// import react from "@vitejs/plugin-react-swc";
+// import autoprefixer from "autoprefixer";
+// import postcss from "postcss/lib/postcss";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     postcss({
+//       plugins: [autoprefixer],
+//       config: "./postcss.config.cjs",
+//     }),
+//   ],
+//   build: {
+//     sourcemap: true,
+//   },
+//   define: {
+//     "process.env": {},
+//   },
+//   resolve: {
+//     alias: {
+//       src: "/src",
+//     },
+//   },
+// });
+
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import autoprefixer from "autoprefixer";
-import postcss from "postcss/lib/postcss";
-import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     react(),
-    postcss({
-      plugins: [autoprefixer],
-      config: "./postcss.config.cjs",
-    }),
   ],
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
   build: {
     sourcemap: true,
-  },
-  define: {
-    "process.env": {},
+    outDir: "client/dist", // Match the publish directory
   },
   resolve: {
     alias: {
       src: "/src",
     },
   },
+  define: {
+    "process.env": {},
+  },
 });
-
