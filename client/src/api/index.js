1dde091d6a66590fcf556fedecf2da8ae3ec91b1
Panther4u <teampanther4@gmail.com>
2024-08-08 14:15:46 +0530

    Update changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,4 +3,4 @@
 // export const API_BASE_URL = 'http://localhost:8000';
 
 
-export const API_BASE_URL = 'https://ecommerce-project-hnwo.onrender.com';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
